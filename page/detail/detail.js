fb225b972cb725dfb62e3d04f547e47845c3ff63
wheato <wheato2010@gmail.com>
2016-09-28 11:05:12 +0800

    增加详情页的loading flex末位补齐

--- page/detail/detail.js
+++ page/detail/detail.js
@@ -1,42 +1,83 @@
 
 var movieData = {};
 
+var movieId = null,
+    isReady = false;
+
 var pageData = {
 
     data: {
-        movie: {}
+        movie: {},
+        hidden: false
     },
 
     onLoad: function (options) {
-
         var that = this;
 
+        wx.setNavigationBarTitle({
+            title: ""
+        });
+
+        that.setData({
+            hidden: false
+        });
+
+        movieId = options.id;
+        
         wx.request({
-            url: 'https://api.douban.com/v2/movie/subject/' + options.id,
+            url: 'https://api.douban.com/v2/movie/subject/' + movieId,
             header: {
                 'Content-Type': 'application/json'
             },
+
             success: function(res) {
                 var data = res.data;
 
                 movieData = {
-                  'title': data['title'],
-                  'average': data['rating']['average'],
-                  'image': data['images']['medium']
+                    'title': data['title'],
+                    'average': data['rating']['average'],
+                    'image': data['images']['medium']
                 };
 
                 that.setData({
-                    movie: movieData
+                    movie: movieData,
+                    hidden: true
                 });
+
+                if(isReady){
+                    wx.setNavigationBarTitle({
+                        title: movieData['title'],
+                        complete: function (){
+                            console.log('set Title');
+                        }
+                    });
+                }
             }
         });
 
     },
 
     onReady: function () {
-        wx.setNavigationBarTitle({
-            title: movieData['title']
-        });
+
+        if(movieData['title']){
+            wx.setNavigationBarTitle({
+                title: movieData['title'],
+                complete: function (){
+                    console.log('set Title');
+                }
+            });
+        } else {
+            isReady = !isReady;
+        }
+
+    },
+
+    onShow: function () {
+
+    },
+    onUnload: function () {
+        isReady = false;
+        movieData = {};
     }
 };
 
--- page/index/index.js
+++ page/index/index.js
@@ -3,26 +3,54 @@ var util = require('../../util/util.js');
 var current = 0;
 var localList = [];
 
+var isLoading = false;
+
 var getData = function () {
+
+    if(isLoading){
+        return;
+    } else {
+        isLoading = true;
+    }
+
     var that = this;
+
     if(current >= 250){
         return false;
     }
-    util.ajax({
+
+    that.setData({
+        hidden: false
+    });
+
+    wx.request({
         url: 'https://api.douban.com/v2/movie/top250?start=' + current,
-        success: function(data){
-            data = JSON.parse(data);
+        header: {
+            'Content-Type': 'application/json'
+        },
+        success: function(res){
+            var renderList = [],
+                data = res.data;
+
+            isLoading = false;
 
             localList = localList.concat(data['subjects']);
 
+            renderList = localList.concat();
+
+            for(var i = 0, len = Math.abs(localList.length % 3 - 3); i < len && len < 3; i++ ){
+                renderList.push({});
+            }
+
             that.setData({
-                listData: localList,
+                listData: renderList,
                 hidden: true
             });
 
             current += data['count'];
         }
     });
+
 };
 var pageData = {
     data: {
